aafffc341a11496664f594a487e3c9c9490d65f3
lucas <lucasimao64@gmail.com>
2021-01-14 23:12:25 -0300

    criando um fibonacci com if else generico

---
+++ fibonacci.js
@@ -0,0 +1,30 @@
+fibonacci = function(enesimo) {
+    if(enesimo == 1){
+        return 1;
+    }else{
+        if(enesimo == 2){
+            return 1;
+        }else{
+            if(enesimo == 3){
+                return 2;
+            }else{
+                if(enesimo == 4){
+                    return 3;
+                } else {
+                    if(enesimo == 5){
+                        return 5;
+                    } else {
+                        if(enesimo = 6){
+                            return 8;
+                        }
+                    } 
+                }    
+            }    
+        }      
+    }
+    // ....
+    // ....
+    // ....
+}
+
+console.log(fibonacci(7));
