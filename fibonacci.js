a25e9fd07e4491b1af4246cd09825c1082fd4e7d
lucas <lucasimao64@gmail.com>
2021-01-14 23:13:53 -0300

    melhorando o fibonacci com if else otimizado

--- fibonacci.js
+++ fibonacci.js
@@ -1,30 +1,24 @@
 fibonacci = function(enesimo) {
-    if(enesimo == 1){
+    if(enesimo == 1)
         return 1;
-    }else{
-        if(enesimo == 2){
-            return 1;
-        }else{
-            if(enesimo == 3){
-                return 2;
-            }else{
-                if(enesimo == 4){
-                    return 3;
-                } else {
-                    if(enesimo == 5){
-                        return 5;
-                    } else {
-                        if(enesimo = 6){
-                            return 8;
-                        }
-                    } 
-                }    
-            }    
-        }      
-    }
-    // ....
-    // ....
-    // ....
+    if(enesimo == 2)
+        return 1;
+    if(enesimo == 3)
+        return 2;
+    if(enesimo == 4)
+        return 3;
+    if(enesimo == 5)
+        return 5;
+    if(enesimo == 6)
+        return 8;
+    if(enesimo == 7)
+        return 13;
+    if(enesimo == 8)
+        return 21;
+    if(enesimo == 9)
+        return 34;
+    if(enesimo == 10)
+        return 55;
 }
 
 console.log(fibonacci(7));
