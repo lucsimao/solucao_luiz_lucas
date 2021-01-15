fddb0c807d8cb49d569c12d497790e28f1f0e5b9
lucas <lucasimao64@gmail.com>
2021-01-14 23:16:11 -0300

    melhorando fibonacci com recursão e operador ternário

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,4 @@
 fibonacci = function(enesimo) {
-    if(enesimo<=2)
-        return 1;
-    return fibonacci(enesimo-1) + fibonacci(enesimo-2);
+    return enesimo <= 2 ? 1 : fibonacci(enesimo-1) + fibonacci(enesimo-2);
 }
-
 console.log(fibonacci(7));
