05455868bb886534ef4a158a41b5c7fcad2f41ff
lucas <lucasimao64@gmail.com>
2021-01-14 23:15:17 -0300

    melhorando fibonacci com iteração sem ifs

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,7 @@
 fibonacci = function(enesimo) {
     let atual = 1;
     let anterior = 1;
-    let fibonacci;
-    if(enesimo == 1)
-        return 1;
-    if(enesimo == 2)
-        return 1;
+    let fibonacci = anterior;
     for(i = 3; i<= enesimo; i++){
         fibonacci = atual + anterior;
         anterior = atual;
